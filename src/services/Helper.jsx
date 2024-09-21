551f71e753ec5ce0cd63c29e13d92c4a10d8a385
YaShDaRji2043 <yashdarji1955@gmail.com>
2024-09-21 15:41:56 +0530

    first commit

--- src/services/Helper.jsx
+++ src/services/Helper.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = axios.create({
-  // baseURL: "https://drive-backend-x6ww.onrender.com",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://drive-backend-x6ww.onrender.com",
+  // baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
